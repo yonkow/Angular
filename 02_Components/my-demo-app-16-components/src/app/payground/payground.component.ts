a3c14f5cb0c0fd777bcc5e7207182232b0c8d5ca
yonkow <42253121+yonkow@users.noreply.github.com>
2024-02-27 22:13:19 +0200

    Add toggle dog image

--- 02_Components/my-demo-app-16-components/src/app/payground/payground.component.ts
+++ 02_Components/my-demo-app-16-components/src/app/payground/payground.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./payground.component.css']
 })
 export class PaygroundComponent {
+  isToggle = false
+  imgUrl = 'https://t3.ftcdn.net/jpg/02/75/98/22/360_F_275982235_xzqfvupzoy03oAiWf7iFTpTcpvVaR4TF.jpg'
+
+  handleClick() {
+    this.isToggle = !this.isToggle
+
+    console.log('clicked!');
+    console.log(this.isToggle);
+
+  }
 
 }
