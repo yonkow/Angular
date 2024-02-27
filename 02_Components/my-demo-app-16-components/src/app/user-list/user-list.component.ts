a4a4f1f1a4e03e53b87282377ae13ea849eeae7e
yonkow <42253121+yonkow@users.noreply.github.com>
2024-02-27 21:58:03 +0200

    add some variables

--- 02_Components/my-demo-app-16-components/src/app/user-list/user-list.component.ts
+++ 02_Components/my-demo-app-16-components/src/app/user-list/user-list.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
+type User = {
+  name: string,
+  age: number,
+}
+
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent {
+  isToggle = false;
 
+  users = [
+    { name: 'Pesho', age: 17 },
+    { name: 'Mitko', age: 30 },
+    { name: 'Ivka', age: 29 },
+    { name: 'Heilun', age: 2 },
+    { name: 'Niki', age: 28 },
+  ] as User[];
+
+  handleClick() {
+    this.isToggle = !this.isToggle
+    
+    console.log('clicked!');
+    console.log(this.isToggle);
+    
+  }
 }
