28403b525e828aa4c4c4fe2ac789722490a3e23c
yonkow <42253121+yonkow@users.noreply.github.com>
2024-02-27 23:08:54 +0200

    train LiveCycle hooks OnInit OnDestroy

--- 02_Components/my-demo-app-16-components/src/app/payground/payground.component.ts
+++ 02_Components/my-demo-app-16-components/src/app/payground/payground.component.ts
@@ -1,25 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-payground',
   templateUrl: './payground.component.html',
   styleUrls: ['./payground.component.css']
 })
-export class PaygroundComponent {
+export class PaygroundComponent implements OnInit, OnDestroy {
+
   isToggle = false
   imgUrl = 'https://t3.ftcdn.net/jpg/02/75/98/22/360_F_275982235_xzqfvupzoy03oAiWf7iFTpTcpvVaR4TF.jpg'
   greenBackground = 'background-green'
+
+  ngOnInit() {
+    console.log('Compopnent Created!');
+  }
+
+  ngAfterViewInit() {
+    console.log('After Init!');
+  }
+
+  ngOnDestroy() {
+    // cleanup on stuff!!
+    console.log('On destroy!');
+  }
+
+  ngDoCheck() {
+    if (this.isToggle == true) {
+      console.log({ isToggle: this.isToggle });
+    }
+  }
 
   handleClick() {
     this.isToggle = !this.isToggle
 
     console.log('clicked!');
-    console.log(this.isToggle);
-
   }
 
   handleInput(usernameValue: string) {
     console.log('username: ', usernameValue);
-    usernameValue = '';
   }
 }
