d425897eb7ab18100f3d656cdccd6d93279b5063
yonkow <42253121+yonkow@users.noreply.github.com>
2024-02-28 20:15:36 +0200

    Create seed.ts

---
+++ 02_Components/article-app/src/app/data/seed.ts
@@ -0,0 +1,32 @@
+const data = [{
+    title: 'Article 1',
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde, asperiores. Temporibus ab quam voluptates eos sit cumque reiciendis, tenetur ut quibusdam adipisci reprehenderit laborum repellat harum impedit quos quod quasi consectetur expedita nemo recusandae corporis. Molestias perferendis corporis quasi. Dignissimos quae beatae accusamus officia sint, eligendi expedita eveniet non ipsam maiores recusandae rem obcaecati nostrum nihil totam iure tempore accusantium.',
+    author: 'Author 1',
+    imageUrl: 'http://ichef.bbci.co.uk/news/976/cpsprodpb/10434/production/_90121666_agreementcartoon.jpg'
+},
+{
+    title: 'Article 2',
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde, asperiores. Temporibus ab quam voluptates eos sit tempore accusantium.',
+    author: 'Author 2',
+    imageUrl: 'http://www.digitalmeetsculture.net/wp-content/uploads/2015/04/article.jpg'
+},
+{
+    title: 'Article 3',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae doloribus illo ratione maiores. Delectus quas modi eius, perspiciatis numquam inventore, labore dicta consectetur, voluptatum rerum accusamus tempora minus maxime reprehenderit similique! Natus facilis dolore, esse expedita sunt aperiam eveniet doloremque ratione atque ipsam, quaerat, maxime eaque nam repellendus quasi fugit laborum beatae vel. Dolores veniam modi ullam eum ut obcaecati dolorum placeat, ad odit quia, accusantium repellendus eius aliquam similique laboriosam. Quas temporibus doloribus corporis quae cum aperiam dicta nemo accusantium inventore omnis? Porro quo, voluptas quidem molestiae nobis dolores fugit odit exercitationem amet enim nisi. Ducimus provident accusantium culpa.',
+    author: 'Author 3',
+    imageUrl: 'http://i.dailymail.co.uk/i/pix/2010/04/17/article-1266852-092DE58A000005DC-112_634x411.jpg'
+},
+{
+    title: 'Article 4',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae doloribus illo ratione maiores. Delectus quas modi eius, perspiciatis numquam inventore, labore dicta consectetur, voluptatum rerum accusamus tempora minus maxime reprehenderit similique! Natus facilis dolore, esse expedita sunt aperiam eveniet doloremque ratione atque ipsam, quaerat, maxime eaque nam repellendus quasi fugit laborum beatae vel. Dolores veniam modi ullam eum ut obcaecati dolorum placeat, ad odit quia, accusantium repellendus eius aliquam similique laboriosam. Quas teestiae nobis dolores fugit odit exercitationem amet enim nisi. Ducimus provident accusantium culpa.',
+    author: 'Author 4',
+    imageUrl: 'https://www.seoclerk.com/pics/518476-1iDFBR1489198900.png'
+},
+{
+    title: 'Article 5',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae doloribus illo ratione maiores. Delectus quas modi eius, perspiciatis numquam inventore, labore dicta consectetur, voluptatum rerum accusamus tempora minus maxime reprehenderit similique! Natus facilis dolore, esse expedita sunt aperiam eveniet doloremque ratione atque ipsam, quaerat, maxime eaque nam repellendus quasi fugit laborum beatae vel. Dolores veniam modi ullam eum ut obcaecati dolorum placeat, ad odit quia, accusantium repellendus eius aliquam similique laboriosam. Quas temporibus doloribus corporis quae cum aperiam dicta nemo accusantium inventore omnis? Porro quo, voluptas quidem molestiae nobis dolores fugit odit exercitationem amet enim nisi. Ducimus provident accusantium culpa.Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae doloribus illo ratione maiores. Delectus quas modi eius, perspiciatis numquam inventore, labore dicta consectetur, voluptatum rerum accusamus tempora minus maxime reprehenderit similique! Natus facilis dolore, esse expedita sunt aperiam eveniet doloremque ratione atque ipsam, quaerat, maxime eaque nam repellendus quasi fugit laborum beatae vel. Dolores veniam modi ullam eum ut obcaecati dolorum placeat, ad odit quia, accusantium repellendus eius aliquam similique laboriosam. Quas temporibus doloribus corporis quae cum aperiam dicta nemo accusantium inventore omnis? Porro quo, voluptas quidem molestiae nobis dolores fugit odit exercitationem amet enim nisi. Ducimus provident accusantium culpa.',
+    author: 'Author 5',
+    imageUrl: 'http://www.stat.columbia.edu/~gelman/teaching/literacy/images/IV.gif'
+}]
+
+export { data };
