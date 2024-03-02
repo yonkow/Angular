2048d994bacdbee80adde172883c0584ccba3d68
yonkow <42253121+yonkow@users.noreply.github.com>
2024-03-02 11:08:29 +0200

    add input user fnc

--- 03_Services_Rjx/my-palyground/src/app/app.component.ts
+++ 03_Services_Rjx/my-palyground/src/app/app.component.ts
@@ -1,21 +1,30 @@
-import { ChangeDetectorRef, Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
 import { User } from './types/user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  // changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   title = 'my-palyground';
   users: User[] = [
-    {name: 'Pesho', age: 20},
-    {name: 'Stoyan', age: 34},
-    {name: 'Stavri', age: 52},
-    {name: 'Kircho', age: 12},
+    { name: 'Pesho', age: 20 },
+    { name: 'Stoyan', age: 34 },
+    { name: 'Stavri', age: 52 },
+    { name: 'Kircho', age: 12 },
   ];
 
+  addUser(inputName: HTMLInputElement, inputAge: HTMLInputElement) {
 
+    const user: User = {
+      name: inputName.value,
+      age: Number(inputAge.value),
+    }
+
+    this.users.push(user)
+  }
 
 
 
