07b0b501fc9ffb19e1e50a7c88d934bbee9f8588
yonkow <42253121+yonkow@users.noreply.github.com>
2024-03-02 18:12:22 +0200

    Add core and shared module, Add header and footer components

--- 04_Workshop_Components/Workshop_Forum/forum-app-angular-components/src/app/app.module.ts
+++ 04_Workshop_Components/Workshop_Forum/forum-app-angular-components/src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, MainComponent],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, SharedModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
