930b507c0af6bed8fc3b9e2ea8069c5dff049c0b
yonkow <42253121+yonkow@users.noreply.github.com>
2024-03-03 15:43:12 +0200

    Routing Guards

---
+++ 05_Angular-Modules-And-Routing/modules_and_routhing/src/app/user/user-detail/user-detail.guard.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable, retry } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+    return this.checkIsLogged(state.url) || this.router.createUrlTree(['/user/list']);
+  }
+
+  checkIsLogged(url: string): boolean {
+    return false;
+  }
+}
--- 05_Angular-Modules-And-Routing/modules_and_routhing/src/app/user/user.module.ts
+++ 05_Angular-Modules-And-Routing/modules_and_routhing/src/app/user/user.module.ts
@@ -4,6 +4,7 @@ import { UserListComponent } from './user-list/user-list.component';
 import { RouterModule } from '@angular/router';
 import { UserDetailComponent } from './user-detail/user-detail.component';
 import { UserResolver } from './user-detail/user-detail.resolver';
+import { AuthGuard } from './user-detail/user-detail.guard';
 
 @NgModule({
   declarations: [UserListComponent, UserDetailComponent],
@@ -17,6 +18,7 @@ import { UserResolver } from './user-detail/user-detail.resolver';
       {
         path: 'user/detail/:id',
         resolve: { user: UserResolver },
+        canActivate: [AuthGuard],
         component: UserDetailComponent,
       },
     ]),
