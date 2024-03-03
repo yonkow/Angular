463c14691291a3482bec4ca923818a54683e77ca
yonkow <42253121+yonkow@users.noreply.github.com>
2024-03-03 12:56:13 +0200

    create user service and User type to fetch the fake data

--- 05_Angular-Modules-And-Routing/modules_and_routhing/src/app/app.module.ts
+++ 05_Angular-Modules-And-Routing/modules_and_routhing/src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRouthingModule } from './app-routhing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
 import { CoreModule } from './core/core.module';
+import { UserModule } from './user/user.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRouthingModule, BrowserAnimationsModule, CoreModule],
+  imports: [
+    BrowserModule,
+    AppRouthingModule,
+    BrowserAnimationsModule,
+    CoreModule,
+    UserModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- 05_Angular-Modules-And-Routing/modules_and_routhing/src/app/user/user.module.ts
+++ 05_Angular-Modules-And-Routing/modules_and_routhing/src/app/user/user.module.ts
@@ -2,14 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserListComponent } from './user-list/user-list.component';
 
-
-
 @NgModule({
-  declarations: [
-    UserListComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [UserListComponent],
+  imports: [CommonModule],
+  exports: [UserListComponent]
 })
-export class UserModule { }
+export class UserModule {}
