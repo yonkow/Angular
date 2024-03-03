8d90ab766db5eacafd4a8d219024313b2b0f6c1d
yonkow <42253121+yonkow@users.noreply.github.com>
2024-03-03 12:02:30 +0200

    Add shared module, core module and material dependency

--- 05_Angular-Modules-And-Routing/modules_and_routhing/src/app/app.module.ts
+++ 05_Angular-Modules-And-Routing/modules_and_routhing/src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRouthingModule } from './app-routhing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRouthingModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
