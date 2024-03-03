da7eb2f25eb5928e1f59f967ce301773d78015fe
yonkow <42253121+yonkow@users.noreply.github.com>
2024-03-03 12:17:56 +0200

    Add global loader

--- 05_Angular-Modules-And-Routing/modules_and_routhing/src/app/app.module.ts
+++ 05_Angular-Modules-And-Routing/modules_and_routhing/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRouthingModule } from './app-routhing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRouthingModule, BrowserAnimationsModule],
+  imports: [BrowserModule, AppRouthingModule, BrowserAnimationsModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- 05_Angular-Modules-And-Routing/modules_and_routhing/src/app/shared/shared.module.ts
+++ 05_Angular-Modules-And-Routing/modules_and_routhing/src/app/shared/shared.module.ts
@@ -3,9 +3,8 @@ import { CommonModule } from '@angular/common';
 import { SpinnerComponent } from './spinner/spinner.component';
 
 @NgModule({
-  declarations: [
-    SpinnerComponent
-  ],
+  declarations: [SpinnerComponent],
   imports: [CommonModule],
+  exports: [SpinnerComponent]
 })
 export class SharedModule {}
