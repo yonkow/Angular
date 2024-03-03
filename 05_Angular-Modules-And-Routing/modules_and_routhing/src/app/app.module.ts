1ec1fd22014fd154c82ece11aca9328e96b4d67d
yonkow <42253121+yonkow@users.noreply.github.com>
2024-03-03 14:22:09 +0200

    add routing

--- 05_Angular-Modules-And-Routing/modules_and_routhing/src/app/app.module.ts
+++ 05_Angular-Modules-And-Routing/modules_and_routhing/src/app/app.module.ts
@@ -2,22 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { AppRouthingModule } from './app-routhing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
 import { CoreModule } from './core/core.module';
 import { UserModule } from './user/user.module';
+import { TodoModule } from './todo/todo.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRouthingModule,
+    AppRoutingModule,
     BrowserAnimationsModule,
     CoreModule,
     UserModule,
     HttpClientModule,
+    TodoModule
   ],
   providers: [],
   bootstrap: [AppComponent],
