f12af4cef778218caf1a667a8b130646dfd72d57
yonkow <42253121+yonkow@users.noreply.github.com>
2024-03-04 10:45:30 +0200

    Add home page view with functionality and conditions

--- 06_Workshop_Modules_and_Routing/Workshop_Forum/forum-app-angular-components/src/app/user/login/login.component.ts
+++ 06_Workshop_Modules_and_Routing/Workshop_Forum/forum-app-angular-components/src/app/user/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,13 +8,11 @@ import { UserService } from '../user.service';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  constructor(private userService: UserService, private router: Router) {}
 
-  constructor (private userService: UserService) {
-
-  }
-
-  login(email: string, password: string):void {
+  login(email: string, password: string): void {
     // TODO: for now we are not handling the data
     this.userService.login();
+    this.router.navigate(['/'])
   }
 }
--- 06_Workshop_Modules_and_Routing/Workshop_Forum/forum-app-angular-components/src/app/user/user-routing.module.ts
+++ 06_Workshop_Modules_and_Routing/Workshop_Forum/forum-app-angular-components/src/app/user/user-routing.module.ts
@@ -9,17 +9,17 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
-    canActivate: [AuthActivate],
+    // canActivate: [AuthActivate],
   },
   {
     path: 'register',
     component: RegisterComponent,
-    canActivate: [AuthActivate],
+    // canActivate: [AuthActivate],
   },
   {
     path: 'profile',
     component: ProfileComponent,
-    canActivate: [AuthActivate],
+    // canActivate: [AuthActivate],
   },
 ];
 
