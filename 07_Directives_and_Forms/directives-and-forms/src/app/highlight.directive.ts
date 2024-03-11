919ef4ac5b0b152de5c9acb3a27884ad108a2c72
yonkow <42253121+yonkow@users.noreply.github.com>
2024-03-11 18:52:58 +0200

    Styles with classes

--- 07_Directives_and_Forms/directives-and-forms/src/app/highlight.directive.ts
+++ 07_Directives_and_Forms/directives-and-forms/src/app/highlight.directive.ts
@@ -7,11 +7,52 @@ export class HighlightDirective implements OnInit {
   constructor(private elRef: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit(): void {
-    console.log(this.elRef.nativeElement);
+    // console.log(this.elRef.nativeElement);
     // ! this is bad practice - Never directly access DOM elements - USE ABSTRACT(renderer)
     // this.elRef.nativeElement.style.background = 'orange';
 
-    // Good practice - Abstract manupulation over DOM
-    this.renderer.setStyle(this.elRef.nativeElement, 'background', 'pink');
+    // * Good practice - Abstract manupulation over DOM
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background', 'pink');
+
+    this.renderer.listen(
+      this.elRef.nativeElement,
+      'mouseenter',
+      this.mouseEnterHandler.bind(this)
+    );
+
+    this.renderer.listen(
+      this.elRef.nativeElement,
+      'mouseleave',
+      this.mouseLeaveHandler.bind(this)
+    );
+  }
+
+  mouseEnterHandler(e: MouseEvent): void {
+    // console.log(e);
+
+    /** Setting styles */
+    // this.renderer.setStyle(
+    //   this.elRef.nativeElement,
+    //   'background-color',
+    //   'orange'
+    // );
+
+    /** Setting classes */
+    this.renderer.addClass(this.elRef.nativeElement, 'highlight')
+  }
+
+  mouseLeaveHandler(e: MouseEvent): void {
+    // console.log('mouse leave triggered');
+
+      /** Setting styles */
+    // this.renderer.setStyle(
+    //   this.elRef.nativeElement,
+    //   'background-color',
+    //   'initial'
+    // );
+
+    /** Setting classes */
+    this.renderer.removeClass(this.elRef.nativeElement, 'highlight')
+
   }
 }
