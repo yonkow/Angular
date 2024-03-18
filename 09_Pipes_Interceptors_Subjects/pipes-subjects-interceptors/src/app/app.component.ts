f6a17d119006a5b1c0bc90d1f6ed8f1d46a3e3a8
yonkow <42253121+yonkow@users.noreply.github.com>
2024-03-18 18:39:47 +0200

    Declare custom reduce pipe

--- 09_Pipes_Interceptors_Subjects/pipes-subjects-interceptors/src/app/app.component.ts
+++ 09_Pipes_Interceptors_Subjects/pipes-subjects-interceptors/src/app/app.component.ts
@@ -3,10 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'pipes-subjects-interceptors';
 
-  user = {name: 'Petko', age: '22'}
+  user = { name: 'Petko', age: '22', list: [1, 2, 3, 4, 5, 6, 7] };
+
+  sum(acc: number, curr: number):number {
+    return acc + curr;
+  }
 }
--- 09_Pipes_Interceptors_Subjects/pipes-subjects-interceptors/src/app/reduce.pipe.ts
+++ 09_Pipes_Interceptors_Subjects/pipes-subjects-interceptors/src/app/reduce.pipe.ts
@@ -1,12 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'reduce'
+  name: 'reduce',
 })
-export class ReducePipe implements PipeTransform {
-
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+export class ReducePipe<T> implements PipeTransform {
+  transform(
+    array: T[],
+    callbackFn: (acc: any, curr: any) => any,
+    initialValue: T
+  ): unknown {
+    // const sum = (acc, curr) => acc + curr;
+    // [1, 2, 3, 4].reduce(callbackFn, 0);
+    return array.reduce(callbackFn, initialValue);
   }
-
 }
